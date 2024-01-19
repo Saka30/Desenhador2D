57f41e2e43cc613d6b5887d19b24db8279d0835d
Rafa Saka <rafaelsakatauskas30@gmail.com>
2024-01-19 13:36:48 -0300

    update

--- json_utils.py
+++ json_utils.py
@@ -29,56 +29,55 @@ class JsonHandler:
                     case 'ponto':
                         coordenada = self.ajustar_coordenadas(primitivo)
                         p = PontoGr(x=coordenada.x, y=coordenada.y, cor=converte(primitivo['cor']),
-                                    width=round(primitivo['esp']/100 * 30))
+                                    width=round(primitivo['esp'] / 100 * 30))
                     case 'reta':
-                        coordenadas = (self.ajustar_coordenadas(primitivo['p1']), self.ajustar_coordenadas(primitivo['p2']))
-                        p = RetaGr(p1=coordenadas[0],p2=coordenadas[1],
-                                   cor=converte(primitivo['cor']), width=round(primitivo['esp']/100 * 30))
+                        coordenadas = (
+                        self.ajustar_coordenadas(primitivo['p1']), self.ajustar_coordenadas(primitivo['p2']))
+                        p = RetaGr(p1=coordenadas[0], p2=coordenadas[1],
+                                   cor=converte(primitivo['cor']), width=round(primitivo['esp'] / 100 * 30))
                     case 'triangulo':
-                        coordenadas = [self.ajustar_coordenadas(primitivo[pt]) for pt in ['p1','p2','p3']]
-                        p = TrianguloGr(coordenadas,cor=converte(primitivo['cor']), width=round(primitivo['esp']/100 * 30))
+                        coordenadas = [self.ajustar_coordenadas(primitivo[pt]) for pt in ['p1', 'p2', 'p3']]
+                        p = TrianguloGr(coordenadas, cor=converte(primitivo['cor']),
+                                        width=round(primitivo['esp'] / 100 * 30))
                     case 'retangulo':
-                        coordenadas = (self.ajustar_coordenadas(primitivo['p1']), self.ajustar_coordenadas(primitivo['p2']))
+                        coordenadas = (
+                        self.ajustar_coordenadas(primitivo['p1']), self.ajustar_coordenadas(primitivo['p2']))
                         p = RetanguloGr(p1=coordenadas[0], p2=coordenadas[1],
-                                        cor=converte(primitivo['cor']), width=round(primitivo['esp']/100 * 30))
+                                        cor=converte(primitivo['cor']), width=round(primitivo['esp'] / 100 * 30))
                     case 'circulo':
                         centro = self.ajustar_coordenadas(primitivo['centro'])
                         raio = primitivo['raio'] * self.canvas.largura
                         p = CirculoGr(centro=centro, raio=raio, cor=converte(primitivo['cor']),
-                                      width=round(primitivo['esp']/100 * 30))
+                                      width=round(primitivo['esp'] / 100 * 30))
 
                     case 'mandala':
                         centro = self.ajustar_coordenadas(primitivo['p1'])
                         p2 = self.ajustar_coordenadas(primitivo['p2'])
 
                         p = Mandala(p1=centro, p2=p2, corCirc=converte(primitivo['cor1']),
-                                    corRetas=converte(primitivo['cor2']), width=round(primitivo['esp']/100 * 30))
+                                    corRetas=converte(primitivo['cor2']), width=round(primitivo['esp'] / 100 * 30))
 
                 if p is not None:
                     array_primitivos.append(p)
 
         return array_primitivos
 
-    def write(self, path:str, ed:LinkedList):
-        desenho = {'desenho':{}}
+    def write(self, path: str, ed: LinkedList) -> None:
+        desenho = {'desenho': {}}
         meu_desenho = desenho['desenho']
 
         for i in range(len(ed)):
             primitivo = ed[i]
-            if primitivo.tipo not in meu_desenho.keys():
-                meu_desenho[primitivo.tipo] = []
-
-            meu_desenho[primitivo.tipo].append(primitivo.info(self.canvas))
+            meu_desenho.setdefault(primitivo.tipo, []).append(primitivo.info(self.canvas))
 
         json_object = json.dumps(desenho, indent=4)
 
         with open(path, 'w') as json_file:
             json_file.write(json_object)
 
-    def ajustar_coordenadas(self,coordenadas: dict) -> Ponto:
+    def ajustar_coordenadas(self, coordenadas: dict) -> Ponto:
 
         x = round(coordenadas['x'] * self.canvas.largura)
         y = round(coordenadas['y'] * self.canvas.altura)
 
         return Ponto(x, y)
-
